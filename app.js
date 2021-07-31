e468ca156d50d548f825ff7be1ebc7fe7469449e
aparkala <aparkala@scu.edu>
2021-07-31 03:27:24 -0700

    Security Fix

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const flash = require('connect-flash');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI =
-'mongodb+srv://akshay:akshayparkala@cluster0.s1fpk.mongodb.net/shop?retryWrites=true&w=majority';
+const MONGODB_URI = // MongoDB-Client URI //
 
 const app = express();
 const store = new MongoDBStore({
